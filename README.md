9f0e5b5b01454d0c4e82f7c0fef9e3d7d775d795
Kocsen <kocsenc@gmail.com>
2016-05-09 15:02:24 -0400

    Integrate ambient module

--- README.md
+++ README.md
@@ -1,15 +1,36 @@
 # heardis
-Tessel2 + Ambient Module to stream dB levels to plot.ly
+
+At it's core this is the code that runs on a Tessel2 which grabs sound 
+data off the ambient sensor and streams it to [plot.ly](https://plot.ly).
+Plot.ly stores the information and can be shared accross devices.
+
+The goal of this project is to be able to see, in real time, the volume
+around the device from anywhere.
+
+The cool thing about Tessels is that they are internet ready; imagine an 
+Arduino that just has Wi-Fi; and it works.
+
+
+## What you'll need
+
+You will need:
+
+1. A Tessel or [Tessel2](https://tessel.io) (preferred)
+1. The [Tessel Ambient Module](https://tessel.io/modules#module-ambient)
+1. A Free account on [plot.ly](https://plot.ly). See `config.json.sample` to see what you need from [plot.ly](https://plot.ly).
 
 ## Get Started
 
+1. Create a `config.json` based off of `config.json.sample` and fill out the missing parts.
 1. `npm install`
-1. `node index.js`
-
-
-`index.js` is the main file that will open a stream. You will need credentials to [plot.ly](https://plot.ly)
+1. Connect to your tessel ([see quickstart](http://tessel.github.io/t2-start/))
+1. `t2 run index.js` to run on tessel 
 
 
 ### Missing config.js?
 
 There is a sample `config.json.example` file to show you what config information looks like.
+But basically you will need an `API key` for plot.ly along with a `STREAM key` for streaming
+data. They are both easy to find in plot.ly's [settings page](https://plot.ly/settings/api).
+
+Remember plot.ly information is public by default. 
--- index.js
+++ index.js
@@ -1,33 +1,66 @@
 var config = require('./config.json');
 var plotly = require('plotly')(config.user, config.apiKey);
+var ambientlib = require('ambient-attx4');
+var tessel = require('tessel'); // does not need to be in node_modules
 
-var initData = [{x: [], y: [], stream: {token: config.streamKey, maxpoints: 1000}}];
-var initGraphOptions = {fileopt: "extend", filename: config.graphName};
-
-plotly.plot(initData, initGraphOptions, function (err, msg) {
-  if (err) return console.log(err);
-  console.log(msg);
-
-  // Create the stream
-  var stream1 = plotly.stream(config.streamKey, function (err, res) {
-    console.log('STREAM ENDING');
-    console.log(err, res);
-    clearInterval(loop); // once stream is closed, stop writing
-  });
-
-  // Set the loop
-  var loop = setInterval(function () {
-    var streamObject = JSON.stringify(
-      {
-        x: (new Date).getTime(),
-        y: getRandomArbitrary(0, 500)
-      }
-    );
-    stream1.write(streamObject + '\n');
-
-  }, 1000);
+var ambient = ambientlib.use(tessel.port[config.ambientModulePort]);
+ambient.on('ready', function () {
+  init()
 });
 
-function getRandomArbitrary(min, max) {
-  return Math.random() * (max - min) + min;
+ambient.on('error', function (err) {
+  console.log(err);
+});
+
+/**
+ * Main function. Will initialize the structure of the data sent
+ * over to plot.ly.
+ * Will then initialize the stream with plot.ly and send
+ * data on every interval.
+ *
+ * Sensor data uses the Tessel Ambient module.
+ */
+function init() {
+  var interval = 1000; // in ms
+  var initData = [{
+    x: [],
+    y: [],
+    stream: {token: config.streamKey, maxpoints: 1000}
+  }];
+  var initGraphOptions = {
+    fileopt: 'extend',
+    filename: config.graphName + (new Date).getTime()
+  };
+
+  plotly.plot(initData, initGraphOptions, function (err, msg) {
+    if (err) return console.log(err);
+    console.log(msg);
+
+    // Create the stream
+    var stream1 = plotly.stream(config.streamKey, function (err, res) {
+      console.log('STREAM ENDING');
+      clearInterval(loop);
+      // if stream is closed, stop writing
+      console.log(err, res);
+    });
+
+    var time = 0;
+    var loop = setInterval(function () {
+      ambient.getSoundLevel(function (err, sound) { // sound is between 0.0 and 1.0
+        if (err) return console.log(err);
+
+        var loudness = (sound * 100.0).toFixed(4);
+        console.log('Sound Level:', loudness, '%');
+        var streamObject = JSON.stringify(
+          {
+            x: time,
+            y: loudness
+          }
+        );
+        stream1.write(streamObject + '\n');
+
+        time++;
+      });
+    }, interval);
+  });
 }
